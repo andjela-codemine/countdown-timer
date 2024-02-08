d2bd28e62684dcae529c0c4ae50513563ad03e97
Andjela Vucic <andjela.vucic@codemine.me>
2024-02-08 16:15:45 +0100

    COD-102: feat: logic for countdown

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const targetDate = new Date('2024-02-22T23:59:59');
+
+setInterval(() => {
+  const now = new Date();
+  const timeDifference = targetDate - now;
+
+  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+  document.documentElement.style.setProperty("--seconds", seconds);
+  document.documentElement.style.setProperty("--minutes", minutes);
+  document.documentElement.style.setProperty("--hours", hours);
+  document.documentElement.style.setProperty("--days", days);
+}, 500);
