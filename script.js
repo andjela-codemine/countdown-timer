e9c511c005e44e9d4207b569be53e9cd8140ea3b
Andjela Vucic <andjela.vucic@codemine.me>
2024-02-11 22:00:43 +0100

    COD-102: feat: card flip

--- script.js
+++ script.js
@@ -1,17 +1,79 @@
+const flipCards = document.querySelectorAll('.flip-card');
+const styles = getComputedStyle(flipCards[0]);
+let days = parseInt(styles.getPropertyValue('--days'));
+let hours = parseInt(styles.getPropertyValue('--hours'));
+let minutes = parseInt(styles.getPropertyValue('--minutes'));
+let seconds = parseInt(styles.getPropertyValue('--seconds'));
 
-const targetDate = new Date('2024-03-12T00:00:59');
+const updateDisplay = () => {
+  flipCards[0].style.setProperty('--days', days);
+  flipCards[1].style.setProperty('--hours', hours);
+  flipCards[2].style.setProperty('--minutes', minutes);
+  flipCards[3].style.setProperty('--seconds', seconds);
+}
 
-setInterval(() => {
-  const now = new Date();
-  const timeDifference = targetDate - now;
+const countdown = () => {
+  if (days === 0 && hours === 0 && minutes === 0 && seconds === 0){
+    return;
+  }
+  
+  if (seconds === 0) {
+    seconds = 59;
 
-  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+    if (minutes === 0) {
+      minutes = 59;
 
-  document.documentElement.style.setProperty("--seconds", seconds);
-  document.documentElement.style.setProperty("--minutes", minutes);
-  document.documentElement.style.setProperty("--hours", hours);
-  document.documentElement.style.setProperty("--days", days);
-}, 500);
+      if (hours === 0) {
+        hours = 23;
+
+        if (days > 0) {
+          setTimeout(() => {
+            startAnimation(0);
+          }, 500);
+          days--;
+        }
+      } else {
+        setTimeout(() => {
+          startAnimation(1);
+        }, 500)
+        hours--;
+      }
+    } else {
+      setTimeout(() => {
+        startAnimation(2);
+      }, 500);
+      minutes--;
+    }
+  } else {
+    setTimeout(() => {
+      startAnimation(3);
+    }, 500);
+    seconds--;
+  }
+  
+  updateDisplay();
+}
+
+const startAnimation = (n) => {
+  const flip = flipCards[n].querySelectorAll('[data-flip]');
+  const top = flip[0];
+  const bottom = flip[1];
+
+  top.dataset.flip = 'true';
+  bottom.dataset.flip = 'true';
+
+  top.addEventListener("animationend", () => {
+    bottom.addEventListener("animationend", () => {
+      top.dataset.flip = 'false';
+      bottom.dataset.flip = 'false';
+    }, { once: true });//remove event listener after once triggered
+  }, { once: true });
+}
+
+
+const startCountdown = () => {
+  countdown();
+  setInterval(countdown, 1000);
+}
+
+startCountdown();
